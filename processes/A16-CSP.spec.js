8f43ee754833f67035e8fbc82766bebe87259239
silverdale-muhammadmohsinmushtaq <mohsin.mushtaq@silverdaletech.com>
2025-10-27 21:14:49 +0500

    Structure

---
+++ processes/A16-CSP.spec.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const { test, expect } = require('@playwright/test');
+
+let page;
+
+test.describe.serial('Odoo End-to-End QA', () => {
+  test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
+    await page.goto(process.env.SERVER_LINK);
+    await page.locator("//input[@name='login']").fill(process.env.ODOO_USERNAME);
+    await page.locator("//input[@name='password']").fill(process.env.ODOO_PASSWORD);
+    await page.getByRole('button', { name: 'Log in', exact: true }).click();
+    await page.getByRole('button', { name: 'User' }).waitFor();
+    
+  });
+
+	test('Verify the user can create "Company" Type contact', async () => {
+		await page.goto(process.env.SERVER_LINK);
+    	
+	});
+
+	
+
+
+
+	test.afterAll(async () => {
+    	await page.goto(process.env.SERVER_LINK);
+    	await page.getByRole('button', { name: 'User' }).click();
+    	await page.getByRole('menuitem', { name: /Log out/i }).click();
+    	await page.close();
+	});
+});
+
